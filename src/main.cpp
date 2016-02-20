26c85799af183c0d2efbb9a253efbec8e276cd9c
William Colin Peters <wcpeters93@gmail.com>
2016-02-19 23:21:32 -0500

    initial setup of project

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
+
+
+int main()
+{
+  // main window
+  GLFWwindow* win = initGraphics();
+
+  // main graphics loop
+  while(!glfwWindowShouldClose(win))
+  {
+
+  }
+}
